1c3113afd90174782fa6feca91b8bc3865325f76
Roberto <r.attanasio90@gmail.com>
2021-03-02 17:36:58 +0100

    Inizio veste grafica

--- main.js
+++ main.js
@@ -1,66 +1,78 @@
-var numeriComputer = [];
-var numeriUtente = [];
-var sceltaDifficolta = parseInt(prompt('Definisci una difficoltà dandomi un numero tra 1 (numeri da 0 a 100), 2 (numeri tra 0 e 80) o 3 (numeri tra 0 e 50).'));
-var livello = 0;
-
-switch (sceltaDifficolta) {
-    case 1:
-        livello = 100;
-        break;
-    case 2:
-        livello = 80;
-        break;
-    case 3:
-        livello = 50;
-        break;
-    default:
-        alert('Hai inserito un livello errato, per questo giocherai al massimo della difficoltà.')
-        livello = 50;
+btnStartGame = document.getElementById('startGame');
+gameItem = document.getElementsByClassName('gameItem');
+for (i = 0; i < gameItem.length; i++) {
+    gameItem[i].innerHTML = '<i class="fas fa-question-circle"></i>';
 }
 
-
-for (i = 0; i < 16; i++) {
-    var generateNumber = randomNumber(livello);
-    // STATE INTERMEDIO PER EVITARE DUPLICATI
-    if (numeriComputer.includes(generateNumber)) {
-        i -= 1;
-    } else {
-        // PUSH NELL'ARRAY
-        numeriComputer.push(generateNumber);
-    }
-}
-
-console.log(numeriComputer);
-
-for (i = 1; i <= numeriComputer.length; i++) {
-    // INSERIMENTO TRAMITE PROMPT 
-    var numeroInserito = parseInt(prompt('Dimmi un numero.'));
-
-    // STATE DI CONTROLLO DEI VALORI IN INGRESSO E POSSIBILE USCITA DAL LOOP 
-    if (((numeroInserito < 0)) || (numeroInserito > livello) || (isNaN(numeroInserito))) {
-        alert('Hai inserito un numero fuori dal range consentito dal tuo livello. Ricominciamo...');
-        break;
-    }
-    if (numeriUtente.includes(numeroInserito)) {
-        alert('Non fare il furbetto, non vale scrivere lo stesso numero! Ricomincia da capo e sii onesto.');
-        break;
+btnStartGame.addEventListener('click', function() {
+    for (i = 0; i < gameItem.length; i++) {
+        gameItem[i].innerHTML = '<i class="fas fa-question-circle"></i>';
     }
 
-    numeriUtente.push(numeroInserito);
-    // console.log(numeriUtente);
+    var numeriComputer = [];
+    var numeriUtente = [];
+    var sceltaDifficolta = document.getElementById('difficolta').value;
 
-
-    // STATO DI SCONFITTA
-    if (numeriComputer.includes(numeroInserito)) {
-        alert('Mi dispiace ma hai perso, ti sei fermato al livello ' + i + '.');
-        break;
+    switch (sceltaDifficolta) {
+        case 'facile':
+            livello = 100;
+            break;
+        case 'medio':
+            livello = 80;
+            break;
+        case 'difficile':
+            livello = 50;
+            break;
     }
 
-    // STATO FINALE IN CASO DI VITTORIA
-    if (i == numeriComputer.length) {
-        alert('Complimenti, hai vinto tu. Hai superato ' + numeriComputer.length + ' livelli!')
+    for (i = 0; i < 16; i++) {
+        var generateNumber = randomNumber(livello);
+        // STATE INTERMEDIO PER EVITARE DUPLICATI
+        if (numeriComputer.includes(generateNumber)) {
+            i -= 1;
+        } else {
+            // PUSH NELL'ARRAY
+            numeriComputer.push(generateNumber);
+        }
     }
-}
+
+    console.log(numeriComputer);
+
+    for (i = 1; i <= numeriComputer.length; i++) {
+        // INSERIMENTO TRAMITE PROMPT 
+        var numeroInserito = parseInt(prompt('Dimmi un numero.'));
+
+        // STATE DI CONTROLLO DEI VALORI IN INGRESSO E POSSIBILE USCITA DAL LOOP 
+        if (((numeroInserito < 0)) || (numeroInserito > livello) || (isNaN(numeroInserito))) {
+            document.getElementById('results').innerHTML = 'Hai inserito un numero fuori dal range consentito dal tuo livello. Ricomincia da capo.';
+
+            break;
+        }
+        if (numeriUtente.includes(numeroInserito)) {
+            document.getElementById('results').innerHTML = 'Non fare il furbetto, non vale scrivere lo stesso numero! Ricomincia da capo e sii onesto.';
+            break;
+        }
+
+        numeriUtente.push(numeroInserito);
+        // console.log(numeriUtente);
+
+
+        // STATO DI SCONFITTA
+        if (numeriComputer.includes(numeroInserito)) {
+            document.getElementById('results').innerHTML = 'Mi dispiace ma hai perso, ti sei fermato al livello ' + i + '.';
+            break;
+        }
+
+        // STATO FINALE IN CASO DI VITTORIA
+        if (i == numeriComputer.length) {
+            document.getElementById('results').innerHTML = 'Complimenti, hai vinto tu. Hai superato ' + numeriComputer.length + ' livelli!';
+            document.getElementsByClassName('game_item').innerHTML = '<i class="fas fa-thumbs-up"></i>';
+
+        }
+    }
+})
+
+
 
 
 // FUNZIONE PER GENERATE NUMBER, LIVELLO COME ARGOMENTO
